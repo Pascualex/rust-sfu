3438c1dcb4388dd94058d53996c20857566aacfb
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2021-12-13 21:13:07 +0100

    add sfu with manual sdp base64 copy

--- client/ts/main.ts
+++ client/ts/main.ts
@@ -3,12 +3,14 @@ window.onload = start;
 async function start() {
     const stream = await getWebcamStream();
     displayWebcamStream(stream);
-    startCall(stream);
+    generateOffer(stream);
 }
 
 async function getWebcamStream() {
     const devicesInfo = await navigator.mediaDevices.enumerateDevices();
+    console.log(devicesInfo);
     const deviceId = devicesInfo.find(x => x.label == "DroidCam Source 3")?.deviceId;
+    if (!deviceId) console.error("DroidCam Source 3 not found");
     return navigator.mediaDevices.getUserMedia({
         video: {
             width: 1280,
@@ -20,41 +22,44 @@ async function getWebcamStream() {
 }
 
 function displayWebcamStream(stream: MediaStream) {
-    const video = document.getElementById("sender-video") as HTMLVideoElement;
+    const video = document.getElementById("outbound-video") as HTMLVideoElement;
     video.srcObject = stream;
 }
 
-async function startCall(stream: MediaStream) {
-    const spc = new RTCPeerConnection();
-    const rpc = new RTCPeerConnection();
-
-    spc.onicecandidate = e => rpc.addIceCandidate(e.candidate || undefined);
-    rpc.onicecandidate = e => spc.addIceCandidate(e.candidate || undefined);
-    const video = document.getElementById("receiver-video") as HTMLVideoElement;
-    rpc.ontrack = e => video.srcObject = e.streams[0];
-
-    spc.addTransceiver(stream.getVideoTracks()[0], {
-        direction: "sendonly",
-        streams: [stream],
-        sendEncodings: [
-            { rid: "h", scaleResolutionDownBy: 1 },
-            { rid: "m", scaleResolutionDownBy: 2 },
-            { rid: "l", scaleResolutionDownBy: 4 },
-        ],
+async function generateOffer(stream: MediaStream) {
+    const pc = new RTCPeerConnection({
+        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
     });
 
-    const offer = await spc.createOffer({
-        offerToReceiveAudio: true,
+    const video = document.getElementById("inbound-video") as HTMLVideoElement;
+    pc.ontrack = ev => video.srcObject = ev.streams[0];
+
+    pc.addTransceiver(stream.getVideoTracks()[0], {
+        direction: "sendonly",
+        streams: [stream],
+        // sendEncodings: [
+        //     { rid: "h", scaleResolutionDownBy: 1 },
+        //     { rid: "m", scaleResolutionDownBy: 2 },
+        //     { rid: "l", scaleResolutionDownBy: 4 },
+        // ],
+    });
+
+    const offer = await pc.createOffer({
+        // offerToReceiveAudio: true,
         offerToReceiveVideo: true,
     });
     updateSPDCodecs(offer);
-    await spc.setLocalDescription(offer);
-    await rpc.setRemoteDescription(offer);
+    await pc.setLocalDescription(offer);
 
-    const answer = await rpc.createAnswer();
-    updateSPDCodecs(answer);
-    await rpc.setLocalDescription(answer);
-    await spc.setRemoteDescription(answer);
+    const offerField = document.getElementById("offer") as HTMLParagraphElement;
+    offerField.textContent = btoa(JSON.stringify(pc.localDescription));
+
+    const answerField = document.getElementById("answer") as HTMLTextAreaElement;
+    const processAnswerButton = document.getElementById("process-answer") as HTMLButtonElement;
+    processAnswerButton.onclick = _ => {
+        const answer = new RTCSessionDescription(JSON.parse(atob(answerField.value)));
+        pc.setRemoteDescription(answer);
+    };
 }
 
 function updateSPDCodecs(desc: RTCSessionDescriptionInit) {
@@ -64,8 +69,8 @@ function updateSPDCodecs(desc: RTCSessionDescriptionInit) {
 
     const idx = lines.findIndex(x => x.startsWith("m=video"));
     const words = lines[idx].split(" ");
-    words[3] = "35"; // av1
-    words[17] = "96"; // vp8
+    // words[3] = "35"; // av1
+    // words[17] = "96"; // vp8
     lines[idx] = words.join(" ");
 
     desc.sdp = lines.join("\n");
